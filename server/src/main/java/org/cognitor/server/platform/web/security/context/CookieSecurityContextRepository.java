f549cb69f7a5404f44a7602c93b088244b7e86b1
Patrick Kranz <patrick@got-email.de>
2013-02-02 16:31:35 +0100

    first version of CookieSecurityContextRepository. encryption and context renewal and valid time still missing

--- server/src/main/java/org/cognitor/server/platform/web/security/context/CookieSecurityContextRepository.java
+++ server/src/main/java/org/cognitor/server/platform/web/security/context/CookieSecurityContextRepository.java
@@ -1,12 +1,21 @@
 package org.cognitor.server.platform.web.security.context;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.security.web.context.HttpRequestResponseHolder;
+import org.springframework.security.web.context.SaveContextOnUpdateOrErrorResponseWrapper;
 import org.springframework.security.web.context.SecurityContextRepository;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.springframework.util.Assert.notNull;
 
 /**
  * This implementation of Springs <code>SecurityContextRepository</code> stores
@@ -24,32 +33,121 @@ import javax.servlet.http.HttpServletResponse;
  * {@link org.springframework.security.web.context.SecurityContextPersistenceFilter}
  * saves the security context <em>after</em> the request has been served.
  *
- * The redirect after submit pattern is recommended here to ease this issue.
+ * To solve this problem the {@link SecurityContextRepositoryResponseWrapper}
+ * which implements {@link SaveContextOnUpdateOrErrorResponseWrapper}
+ * is used so that on <code>sendError()</code> and <code>sendRedirect</code>
+ * the SecurityContext is persisted to the cookie. Therefore the send redirect
+ * after submit pattern should deal with issue.
  *
  * @author Patrick Kranz
  */
 public class CookieSecurityContextRepository implements SecurityContextRepository {
     public static final String DEFAULT_COOKIE_NAME = "context";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CookieSecurityContextRepository.class);
+    private static final String COOKIE_DATA_ENCODING = "UTF-8";
+
+    private final SecurityContextSerializer cookieSerializer;
+    private final String domainName;
+
     private String cookieName = DEFAULT_COOKIE_NAME;
+
+    public CookieSecurityContextRepository(SecurityContextSerializer serializer,
+                                           String domainName) {
+        notNull(serializer);
+        notNull(domainName);
+        this.cookieSerializer = serializer;
+        this.domainName = domainName;
+    }
 
     @Override
     public SecurityContext loadContext(HttpRequestResponseHolder requestResponseHolder) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        wrapResponse(requestResponseHolder);
+        LOGGER.debug("Trying to load security context from request.");
+
+        Cookie securityCookie = getCookieForName(requestResponseHolder.getRequest().getCookies(),
+                cookieName);
+
+        if (noSecurityCookieFound(securityCookie)) {
+            LOGGER.debug("No security cookie found. Returning new context.");
+            return createNewContext();
+        }
+
+        return getSecurityContext(securityCookie);
+    }
+
+    private void wrapResponse(HttpRequestResponseHolder requestResponseHolder) {
+        requestResponseHolder.setResponse(new SecurityContextRepositoryResponseWrapper(
+                requestResponseHolder.getRequest(), requestResponseHolder.getResponse(), this));
+    }
+
+    private boolean noSecurityCookieFound(Cookie securityCookie) {
+        return securityCookie == null;
+    }
+
+    private SecurityContext getSecurityContext(Cookie securityCookie) {
+        try {
+            SecurityContext context =
+                    cookieSerializer.deserialize(decodeBase64(securityCookie.getValue()));
+            LOGGER.debug("Successfully loaded security context from cookie");
+            return context;
+        } catch (SerializeException exception) {
+            return createNewContext();
+        }
+    }
+
+
+    private static byte[] decodeBase64(String base64encodedData) {
+        try {
+            return Base64.decode(base64encodedData.getBytes(COOKIE_DATA_ENCODING));
+        } catch (UnsupportedEncodingException exception) {
+            LOGGER.error("No " + COOKIE_DATA_ENCODING + " support found on this system! Login will not work.", exception);
+            throw new SerializeException("Cookie data cannot be decoded", exception);
+        }
+    }
+
+    private static SecurityContext createNewContext() {
+        return SecurityContextHolder.createEmptyContext();
     }
 
     @Override
     public void saveContext(SecurityContext context, HttpServletRequest request, HttpServletResponse response) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        byte[] serializedData = null;
+        try {
+            serializedData = cookieSerializer.serialize(context);
+            String encodedData = encodeBase64(serializedData);
+            addCookieToResponse(response, encodedData);
+        } catch (SerializeException exception) {
+            LOGGER.error("Serialization of security context failed. No cookie set.", exception);
+        }
+    }
+
+    private void addCookieToResponse(HttpServletResponse response, String encodedData) {
+        Cookie securityCookie = new Cookie(cookieName, encodedData);
+        securityCookie.setDomain(domainName);
+        securityCookie.setSecure(true);
+        response.addCookie(securityCookie);
+    }
+
+    private String encodeBase64(byte[] serializedData) {
+        try {
+            byte[] base64encodedData = Base64.encode(serializedData);
+            return new String(base64encodedData, COOKIE_DATA_ENCODING);
+        } catch (UnsupportedEncodingException exception) {
+            LOGGER.error("No " + COOKIE_DATA_ENCODING + " support found on this system! Login will not work.", exception);
+            return "";
+        }
     }
 
     @Override
     public boolean containsContext(HttpServletRequest request) {
-        final Cookie[] cookies = request.getCookies();
-        return cookies != null && getCookieForName(cookies, cookieName) != null;
+        return getCookieForName(request.getCookies(), cookieName) != null;
     }
 
     private static Cookie getCookieForName(Cookie[] cookies, String cookieName) {
+        if (cookies == null) {
+            return null;
+        }
         for (Cookie cookie : cookies) {
             if (cookieName.equals(cookie.getName())) {
                 return cookie;
--- server/src/test/java/org/cognitor/server/platform/web/security/context/CookieSecurityContextRepositoryTest.java
+++ server/src/test/java/org/cognitor/server/platform/web/security/context/CookieSecurityContextRepositoryTest.java
@@ -3,16 +3,26 @@ package org.cognitor.server.platform.web.security.context;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.web.context.HttpRequestResponseHolder;
+import org.springframework.security.web.context.SaveContextOnUpdateOrErrorResponseWrapper;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import static org.cognitor.server.platform.web.security.context.CookieSecurityContextRepository.DEFAULT_COOKIE_NAME;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Patrick Kranz
@@ -20,13 +30,21 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class CookieSecurityContextRepositoryTest {
     @Mock
-    HttpServletRequest requestMock;
+    private HttpServletRequest requestMock;
 
+    @Mock
+    private HttpServletResponse responseMock;
+
+    @Mock
+    private SecurityContextSerializer cookieSerializerMock;
+
+    private HttpRequestResponseHolder holder;
     private CookieSecurityContextRepository repository;
 
     @Before
     public void setUp() {
-        this.repository = new CookieSecurityContextRepository();
+        holder = new HttpRequestResponseHolder(requestMock, responseMock);
+        this.repository = new CookieSecurityContextRepository(cookieSerializerMock, "localhost");
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -39,6 +57,7 @@ public class CookieSecurityContextRepositoryTest {
         repository.setCookieName("");
     }
 
+    // Contains method
     @Test
     public void shouldReturnFalseWhenRequestWithNoCookiesGiven() {
         when(requestMock.getCookies()).thenReturn(null);
@@ -62,5 +81,119 @@ public class CookieSecurityContextRepositoryTest {
         repository.setCookieName("security");
         when(requestMock.getCookies()).thenReturn(new Cookie[]{ new Cookie("security", "value")});
         assertTrue(repository.containsContext(requestMock));
+    }
+
+    // Constructor
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenNullValueForSerializerGiven() {
+        new CookieSecurityContextRepository(null, "localhost");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenNullValueForDomainGiven() {
+        new CookieSecurityContextRepository(cookieSerializerMock, null);
+    }
+
+    // load context
+    @Test
+    public void shouldWrapResponseWhenHttpRequestResponseHolderGiven() {
+        repository.loadContext(holder);
+        assertTrue(SaveContextOnUpdateOrErrorResponseWrapper.class.isAssignableFrom(holder.getResponse().getClass()));
+    }
+
+    @Test
+    public void shouldReturnEmptySecurityContextWhenNoSecurityCookieGiven() {
+        SecurityContext context = repository.loadContext(holder);
+        assertNotNull(context);
+        assertNull(context.getAuthentication());
+    }
+
+    @Test
+    public void shouldReturnEmptySecurityContextWhenDeserializingThrowsException() {
+        when(requestMock.getCookies()).thenReturn(new Cookie[] { new Cookie("context", "") });
+        when(cookieSerializerMock.deserialize(any(byte[].class))).thenThrow(new SerializeException("test"));
+        SecurityContext context = repository.loadContext(holder);
+        assertNotNull(context);
+        assertNull(context.getAuthentication());
+    }
+
+    @Test
+    public void shouldReturnEmptySecurityContextWhenNoBase64EncodedCookieContentGiven() {
+        when(requestMock.getCookies()).thenReturn(new Cookie[] { new Cookie("context", "") });
+        when(cookieSerializerMock.deserialize(any(byte[].class))).thenAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                byte[] serializedContent = (byte[]) invocation.getArguments()[0];
+                if (serializedContent.length == 0) {
+                    throw new SerializeException("no input data");
+                } else {
+                    fail("expected empty byte[]");
+                    return null;
+                }
+            }
+        });
+        SecurityContext context = repository.loadContext(holder);
+        assertNotNull(context);
+        assertNull(context.getAuthentication());
+    }
+
+    @Test
+    public void shouldReturnDeserializedContextWhenValidBase64EncodedDataGiven() {
+        final SecurityContext validContext = new SecurityContextImpl();
+        byte[] validData = Base64.encode("hello".getBytes());
+        when(cookieSerializerMock.deserialize(any(byte[].class))).thenAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                byte[] serializedContent = (byte[]) invocation.getArguments()[0];
+                String value = new String(serializedContent);
+                if ("hello".equals(value)) {
+                    return validContext;
+                } else {
+                    fail("should have been a valid context");
+                    return null;
+                }
+            }
+        });
+        when(requestMock.getCookies()).thenReturn(new Cookie[]{
+                new Cookie("context", new String(validData))});
+
+        SecurityContext result = repository.loadContext(holder);
+        assertEquals(validContext, result);
+    }
+
+    // save context
+    @Test
+    public void shouldAttachSecurityCookieToResponseWhenSecurityContextGiven() {
+        when(cookieSerializerMock.serialize(any(SecurityContext.class))).thenReturn(new byte[] { 1, 2, 3 });
+        ArgumentCaptor<Cookie> cookieArgumentCaptor = ArgumentCaptor.forClass(Cookie.class);
+        repository.saveContext(new SecurityContextImpl(), requestMock, responseMock);
+        verify(responseMock, atLeastOnce()).addCookie(cookieArgumentCaptor.capture());
+        Cookie securityCookie = cookieArgumentCaptor.getValue();
+        assertEquals(DEFAULT_COOKIE_NAME, securityCookie.getName());
+        assertTrue(securityCookie.getSecure());
+        assertEquals("localhost", securityCookie.getDomain());
+        assertNotNull(securityCookie.getValue());
+    }
+
+    @Test
+    public void shouldEncodeDataBase64WhenSecurityContextGiven() {
+        final byte[] serializedContext = {1, 2, 3};
+        when(cookieSerializerMock.serialize(any(SecurityContext.class))).thenReturn(serializedContext);
+        ArgumentCaptor<Cookie> cookieArgumentCaptor = ArgumentCaptor.forClass(Cookie.class);
+        repository.saveContext(new SecurityContextImpl(), requestMock, responseMock);
+        verify(responseMock, atLeastOnce()).addCookie(cookieArgumentCaptor.capture());
+        String cookieContent = cookieArgumentCaptor.getValue().getValue();
+        byte[] cookieDecoded = Base64.decode(cookieContent.getBytes());
+        assertArrayEquals(serializedContext, cookieDecoded);
+    }
+
+    @Test
+    public void shouldNotAttachCookieToResponseWhenSerializationExceptionGiven() {
+        SecurityContextImpl securityContext = new SecurityContextImpl();
+        when(cookieSerializerMock.serialize(securityContext))
+                .thenThrow(new SerializeException("test"));
+        repository.saveContext(securityContext, requestMock, responseMock);
+
+        verify(responseMock, never()).addCookie(any(Cookie.class));
     }
 }
