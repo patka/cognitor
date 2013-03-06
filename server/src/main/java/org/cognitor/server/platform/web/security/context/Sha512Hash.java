c6e40f7835ee3bbfbb3a50709397fdf2ffdced62
Patrick Kranz <patrick@got-email.de>
2013-03-06 21:12:13 +0100

    and removed the unused import

--- server/src/main/java/org/cognitor/server/platform/web/security/context/Sha512Hash.java
+++ server/src/main/java/org/cognitor/server/platform/web/security/context/Sha512Hash.java
@@ -2,7 +2,6 @@ package org.cognitor.server.platform.web.security.context;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
