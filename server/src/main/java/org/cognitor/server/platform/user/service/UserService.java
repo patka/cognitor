995006fd8128ef503b3823277e67237b2aa607dc
Patrick Kranz <patrick@got-email.de>
2013-08-25 11:40:08 +0200

    removed file template header

--- server/src/main/java/org/cognitor/server/platform/user/service/UserService.java
+++ server/src/main/java/org/cognitor/server/platform/user/service/UserService.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.cognitor.server.platform.user.service;
 
 import org.cognitor.server.platform.user.domain.User;
